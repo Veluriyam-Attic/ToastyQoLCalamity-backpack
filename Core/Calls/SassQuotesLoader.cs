ea1150c700a8b84c70c710be5dd9660aaa53f627
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2024-09-17 21:43:20 -0700

    sass localization

--- Core/Calls/SassQuotesLoader.cs
+++ Core/Calls/SassQuotesLoader.cs
@@ -28,21 +28,20 @@ namespace ToastyQoLCalamity.Core.Calls
                 Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Calamity.2") +
                 Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Calamity.3"));
 
-            AddBossSpecificSassQuote(ModContent.NPCType<Cryogen>(), ["You need to chill out."]);
+            AddBossSpecificSassQuote(ModContent.NPCType<Cryogen>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Cryogen")]);
             AddBossSpecificSassQuote(ModContent.NPCType<Yharon>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Yharon")]);
-            AddBossSpecificSassQuote(ModContent.NPCType<SupremeCalamitas>(), [ "I bet you're going to go spam ping someone in #nohit-discussion now aren't you. Spoiler: No one cares.", 
-                "Another BH1 death?" ]);
+            AddBossSpecificSassQuote(ModContent.NPCType<SupremeCalamitas>(), [ Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Scal"), Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.ScalAlt")]);
             AddBossSpecificSassQuote(ModContent.NPCType<Apollo>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Draedon")]);
             AddBossSpecificSassQuote(ModContent.NPCType<Artemis>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Draedon")]);
             AddBossSpecificSassQuote(ModContent.NPCType<AresBody>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Draedon")]);
             AddBossSpecificSassQuote(ModContent.NPCType<ThanatosHead>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Draedon")]);
-            AddBossSpecificSassQuote(ModContent.NPCType<HiveMind>(), ["'Hive' got a plan for you: Give up."]);
-            AddBossSpecificSassQuote(ModContent.NPCType<PlaguebringerGoliath>(), ["'Hive' got a plan for you: Give up."]);
-            AddBossSpecificSassQuote(ModContent.NPCType<Crabulon>(), ["I better not see this RRed 10 times..."]);
-            AddBossSpecificSassQuote(ModContent.NPCType<AquaticScourgeHead>(), ["Excessive Salt can lead to high blood pressure."]);
-            AddBossSpecificSassQuote(ModContent.NPCType<Leviathan>(), ["Excessive Salt can lead to high blood pressure.", "Don't fish for compliments."]);
-            AddBossSpecificSassQuote(ModContent.NPCType<Anahita>(), ["Excessive Salt can lead to high blood pressure.", "Don't fish for compliments."]);
-            AddBossSpecificSassQuote(ModContent.NPCType<OldDuke>(), ["Don't fish for compliments.", "Double tap to dash!"]);
+            AddBossSpecificSassQuote(ModContent.NPCType<HiveMind>(), [Language.GetTextValue($"Mods.ToastyQoL.SassQuotes.Specific.QueenBee")]);
+            AddBossSpecificSassQuote(ModContent.NPCType<PlaguebringerGoliath>(), [Language.GetTextValue($"Mods.ToastyQoL.SassQuotes.Specific.QueenBee")]);
+            AddBossSpecificSassQuote(ModContent.NPCType<Crabulon>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Crabulon")]);
+            AddBossSpecificSassQuote(ModContent.NPCType<AquaticScourgeHead>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Salt")]);
+            AddBossSpecificSassQuote(ModContent.NPCType<Leviathan>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Salt"), Language.GetTextValue($"Mods.ToastyQoL.SassQuotes.Specific.Fishron")]);
+            AddBossSpecificSassQuote(ModContent.NPCType<Anahita>(), [Language.GetTextValue($"Mods.ToastyQoLCalamity.SassQuotes.Salt"), Language.GetTextValue($"Mods.ToastyQoL.SassQuotes.Specific.Fishron")]);
+            AddBossSpecificSassQuote(ModContent.NPCType<OldDuke>(), [Language.GetTextValue($"Mods.ToastyQoL.SassQuotes.Specific.Fishron"), Language.GetTextValue($"Mods.ToastyQoL.SassQuotes.FishronAlt")]);
         }
     }
 }
